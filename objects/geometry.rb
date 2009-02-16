56da357b8af308e3399117011e2a78e5eccc1c8c
Evan Senter <evansenter@gmail.com>
2009-02-15 23:41:51 -0800

    Melkman's Algorithm has shitty documentation. Gift wrapping will hopefully be easier to get going, and the added complexity isn't too concerning with such a small collection size. I can always come back to it later on, if necessary.

--- objects/geometry.rb
+++ objects/geometry.rb
@@ -2,56 +2,8 @@ require File.join("#{File.dirname(__FILE__)}", "extensions.rb")
 require "matrix"
 
 class Geometry
-  # Input: a  simple polyline W with n vertices V[i]
-  # 
-  # Put first 3 vertices onto deque D so that:
-  # a) 3rd vertex V[2] is at bottom and top of D
-  # b) on D they form a counterclockwise (ccw) triangle
-  # 
-  # While there are more polyline vertices of W to process
-  # Get the next vertex V[i]
-  # {
-  #     Note that:
-  #     a) D is the convex hull of already processed vertices
-  #     b) D[bot] = D[top] = the last vertex added to D
-  # 
-  #     // Test if V[i] is inside D (as a polygon)
-  #     If V[i] is left of D[bot]D[bot+1] and D[top-1]D[top]
-  #         Skip V[i] and Continue with the next vertex
-  # 
-  #     // Get the tangent to the bottom
-  #     While V[i] is right of D[bot]D[bot+1]
-  #         Remove D[bot] from the bottom of D
-  #     Insert V[i] at the bottom of D
-  # 
-  #     // Get the tangent to the top
-  #     While V[i] is right of D[top-1]D[top]
-  #         Pop D[top] from the top of D
-  #     Push V[i] onto the top of D
-  # }
-  # 
-  # Output: D = the ccw convex hull of W.
-
-  def self.convex_hull(points)
-    deque = initialize_deque(*points.slice!(0..2))
-    
-    while !points.empty?
-      if current_point = point_inside_hull?(points.shift)
-      end
-    end
-  end
-  
-  def self.initialize_deque(point_1, point_2, point_3)
-    deque = [point_1, point_2, point_3]
-    deque[1], deque[2] = deque[2], deque[1] if clockwise?(*deque)
-    deque.insert(0, deque.last)
-  end
-  
-  def self.point_inside_hull?(point, deque)
-    point unless clockwise?(point, *deque[0..1]) && clockwise?(point, *deque[-2..-1])
-  end
-
   def self.clockwise?(point_1, point_2, point_3)
+    # Returns true if the points make a clockwise turn, and false if they are all on a line, or turn counter clockwise.
     matrix = Matrix[
       [point_1.x.value, point_1.y.value, 1],
       [point_2.x.value, point_2.y.value, 1],
--- tests/geometry_test.rb
+++ tests/geometry_test.rb
@@ -45,105 +45,6 @@ class GeometryTest < Test::Unit::TestCase
     assert_equal false, Geometry.clockwise?(create_point(0, 0), create_point(0, 0), create_point(0, 0))
   end
   
-  def test_left_of_line__line_first_in_params
-    # Point locations: 1  3
-    #                     2
-    
-    line  = [create_point(0, 0), create_point(50, 50)]
-    point = create_point(50, 0)
-    assert_equal false, Geometry.clockwise?(line.first, line.last, point)
-    
-    # Point locations: 1
-    #                  3  2
-    
-    line  = [create_point(0, 0), create_point(50, 50)]
-    point = create_point(0, 50)
-    assert_equal true, Geometry.clockwise?(line.first, line.last, point)
-  end
-     
-  def test_left_of_line__point_first_in_params
-    # Point locations: 2  1
-    #                     3
-    
-    point = create_point(50, 0)
-    line  = [create_point(0, 0), create_point(50, 50)]
-    assert_equal false, Geometry.clockwise?(point, line.first, line.last)
-    
-    # Point locations: 2
-    #                  1  3
-    
-    point = create_point(0, 50)
-    line  = [create_point(0, 0), create_point(50, 50)]
-    assert_equal true, Geometry.clockwise?(point, line.first, line.last)
-  end
-  
-  def test_point_inside_hull__point_in_hull
-    # Point locations:    1
-    #                     4
-    #                  3     2
-    
-    point_1 = create_point(25, 0)
-    point_2 = create_point(50, 50)
-    point_3 = create_point(0,  50)
-    deque   = [point_3, point_1, point_2, point_3]
-    
-    point_4 = create_point(25, 25)
-    
-    assert_nil Geometry.point_inside_hull?(point_4, deque)
-  end
-  
-  def test_point_inside_hull__point_on_hull
-    # Point locations: 1
-    #                  4
-    #                  3     2
-
-    point_1 = create_point(0,  0)
-    point_2 = create_point(50, 50)
-    point_3 = create_point(0,  50)
-    deque   = [point_3, point_1, point_2, point_3]
-    
-    point_4 = create_point(0, 25)
-    
-    assert_equal point_4, Geometry.point_inside_hull?(point_4, deque)
-  end
-  
-  def test_point_inside_hull__point_outside_hull
-    # Point locations:    1
-    #                  4
-    #                  3     2
-    
-    point_1 = create_point(25, 0)
-    point_2 = create_point(50, 50)
-    point_3 = create_point(0,  50)
-    deque   = [point_3, point_1, point_2, point_3]
-    
-    point_4 = create_point(0, 25)
-    
-    assert_equal point_4, Geometry.point_inside_hull?(point_4, deque)
-  end
-      
-  def test_point_inside_hull__test_conditional_logic
-    Geometry.stubs(:clockwise?).returns(false, false)
-    assert_equal :point, Geometry.point_inside_hull?(:point, [])
-    
-    Geometry.stubs(:clockwise?).returns(true, false)
-    assert_equal :point, Geometry.point_inside_hull?(:point, [])
-    
-    Geometry.stubs(:clockwise?).returns(false, true)
-    assert_equal :point, Geometry.point_inside_hull?(:point, [])
-    
-    Geometry.stubs(:clockwise?).returns(true, true)
-    assert_nil Geometry.point_inside_hull?(:point, [])
-  end
-      
-  def test_initialize_deque
-    Geometry.expects(:clockwise?).with(:point_1, :point_2, :point_3).returns(false)
-    assert_equal [:point_3, :point_1, :point_2, :point_3], Geometry.initialize_deque(:point_1, :point_2, :point_3)
-
-    Geometry.expects(:clockwise?).with(:point_1, :point_2, :point_3).returns(true)
-    assert_equal [:point_2, :point_1, :point_3, :point_2], Geometry.initialize_deque(:point_1, :point_2, :point_3)
-  end
-  
   protected
   
   def create_point(x, y)
@@ -151,5 +52,11 @@ class GeometryTest < Test::Unit::TestCase
       Trait.new(:x, (0...100), :default => x),
       Trait.new(:y, (0...100), :default => y)
     )
+  end
+  
+  def format_points_in(array)
+    array.map do |point|
+      "(#{point.x.value}, #{point.y.value})"
+    end.join(", ")
   end
 end
