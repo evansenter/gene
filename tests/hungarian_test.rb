8a959c506c4ac1b86c8ac0113dbd812f46533e05
Evan Senter <evansenter@gmail.com>
2009-02-25 08:58:50 -0800

    Added a test for find_uncovered_zero in the cab.

--- tests/hungarian_test.rb
+++ tests/hungarian_test.rb
@@ -17,6 +17,18 @@ class TraitTest < Test::Unit::TestCase
     expected = [[0, 1, 2], [1, 0, 1], [0, 0, 0]]
     assert_equal expected, hungarian.send(:minimize_rows)
   end
+
+  def test_find_uncovered_zero
+    hungarian = Hungarian.new([[0, 0], [0, 0]])
+    hungarian.instance_variable_set(:@covered, {:rows => [false, true], :columns => [false, true]})
+    assert_equal [0, 0], hungarian.send(:find_uncovered_zero)
+    
+    hungarian = Hungarian.new([[0, 0], [0, 0]])
+    hungarian.instance_variable_set(:@covered, {:rows => [true, false], :columns => [true, false]})
+    assert_equal [1, 1], hungarian.send(:find_uncovered_zero)
+  end
+  
+  # Write test to ensure non-zero cells are skipped, and a test for the default value
   
   def test_location_covered
     @hungarian.instance_variable_set(:@covered, {:rows => [false], :columns => [false]})
