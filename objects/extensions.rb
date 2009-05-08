5ff9dcb385a207f351fe98f9ceea8bd81c9da889
Evan Senter <evansenter@gmail.com>
2009-05-08 11:42:13 -0700

    Added more extensions and tests.

--- objects/extensions.rb
+++ objects/extensions.rb
@@ -4,15 +4,17 @@ def returning(value)
 end
 
 def add_bounding_methods_to(range)
-  returning(range) do |object|
-    object.instance_eval do
-      def min
-        self.begin.is_a?(Float) ? self.begin : super
-      end
-      
-      def max
-        self.end.is_a?(Float) ? self.end : super
-      end
+  :min[range] = lambda { self.begin }
+  :max[range] = lambda { self.exclude_end? && self.end.is_a?(Integer) ? self.end - 1 : self.end }
+end
+
+class Object
+  def send_if(condition, method, *arguments)
+    block = arguments.pop if arguments.last.is_a?(Proc) && arguments.length == 1
+    if condition
+      block ? eval("self.#{method} &block") : self.send(method, *arguments)
+    else
+      self
     end
   end
 end
@@ -21,16 +23,32 @@ class Symbol
   def to_proc
     proc { |*args| args.first.send(self, *args[1..-1]) }
   end
+  
+  def [](object)
+    object.method(self)
+  end
+  
+  def []=(object, function)
+    symbol = self
+    eigenclass = (class << object; self; end)
+    eigenclass.instance_eval { define_method(symbol, function) }
+  end
+  
+  # def ax_b(a, b, *x)
+  #   (:+[b] * :*[a]) | x
+  # end
+  # ax_b(2, 1, 1, 2, 3)
+  # => [3, 5, 7]
 end
 
-class Object
-  # Not tested.
-  def send_if(condition, method, *args)
-    block = args.shift if args.last.is_a?(Proc) && args.length == 1
-    if condition
-      block ? eval("self.#{method} &block") : self.send(method, *args)
-    else
-      self
-    end
+class Module
+  alias [] instance_method
+  
+  def []=(symbol, function)
+    self.instance_eval { define_method(symbol, function) }
   end
+end
+
+class UnboundMethod
+  alias [] bind
 end
--- tests/extensions_test.rb
+++ tests/extensions_test.rb
@@ -17,11 +17,11 @@ class ExtensionsTest < Test::Unit::TestCase
     add_bounding_methods_to(inclusive_range)
     add_bounding_methods_to(exclusive_range)
     
-    assert_equal 10, inclusive_range.max
     assert_equal 0,  inclusive_range.min
+    assert_equal 10, inclusive_range.max
 
-    assert_equal 9,  exclusive_range.max
     assert_equal 0,  exclusive_range.min
+    assert_equal 9,  exclusive_range.max
   end  
   
   def test_add_bounding_methods_to__float_range__max_and_min
@@ -31,15 +31,50 @@ class ExtensionsTest < Test::Unit::TestCase
     add_bounding_methods_to(inclusive_range)
     add_bounding_methods_to(exclusive_range)
     
-    assert_equal 1.0, inclusive_range.max
     assert_equal 0.0, inclusive_range.min
+    assert_equal 1.0, inclusive_range.max
 
-    assert_equal 1.0, exclusive_range.max
     assert_equal 0.0, exclusive_range.min
+    assert_equal 1.0, exclusive_range.max
   end
   
-  def test_symbol_to_proc
+  def test_object__send_if
+    assert_equal 3, [1, 2, 3].send_if(true, :length)
+    assert_equal [1, 2, 3], [1, 2, 3].send_if(false, :length)
+    
+    assert_equal 1, [1, 2, 3].send_if(true, :[], 0)
+    assert_equal [1, 2, 3], [1, 2, 3].send_if(false, :[], 0)
+    
+    assert_equal [2, 3, 4], [1, 2, 3].send_if(true, :map, proc { |element| element.succ })
+    assert_equal [1, 2, 3], [1, 2, 3].send_if(false, :map, proc { |element| element.succ })
+  end
+  
+  def test_symbol__to_proc
     arrays = [Array.new(0), Array.new(1), Array.new(2)]
     assert_equal [0, 1, 2], arrays.map(&:size)
+  end
+  
+  def test_symbol__array_get
+    dashes = :*["-"]
+    assert_equal "----------", dashes[10]
+  end
+  
+  def test_symbol__array_set
+    string = "Hello world."
+    :singleton[string] = lambda { "You have found my singleton method." }
+    assert_equal "You have found my singleton method.", string.singleton
+  end
+  
+  def test_module__array_get
+    assert_equal "#<UnboundMethod: String#reverse>", String[:reverse].inspect
+  end
+  
+  def test_module__array_set
+    String[:backwards] = lambda { reverse }
+    assert_equal "syawedis", "sideways".backwards
+  end
+  
+  def test_unbound_method__array_get
+    assert_equal "syawedis", String[:reverse]["sideways"][]
   end
 end
