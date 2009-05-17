ee2133b700419d975daba5b141fd2cda6f2236fc
Evan Senter <evansenter@gmail.com>
2009-05-17 16:08:09 -0700

    Started adding methods to petri.rb

--- objects/petri.rb
+++ objects/petri.rb
@@ -3,7 +3,17 @@ require File.join("#{File.dirname(__FILE__)}", "chromosome.rb")
 class Chromosome
   attr_reader :dish
   
-  def initialize(config = {})
+  def initialize(num_organisms, num_genes, num_points, image_dimensions)
+    @dish = (0...num_organisms).map do
+      Chromosome.new(num_genes, num_points, image_dimensions)
+    end
+  end
+  
+  def sort_by_fitness
+    @dish.sort! { |a, b| a.fitness <=> b.fitness }
+  end
+  
+  def update_fitness
     
   end
 end
