9e0c3dd48f9836ba2971c28b0fbdd1bd67892403
Evan Senter <evansenter@gmail.com>
2009-11-19 22:55:35 -0800

    Ghetto code in overlord.rb

--- test/overlord.rb
+++ test/overlord.rb
@@ -1,17 +1,2 @@
 # Run the tests! All of them!
 Dir.glob(File.join(File.dirname(__FILE__), "..", "test", "*_test.rb")).each { |test_file| print `ruby #{test_file}` }
-
-git add	initializers/                            &&   
-git add	lib/calculator.rb                        &&   
-git add	lib/geometry.rb                          &&   
-git add	lib/hungarian.rb                         &&   
-git add	scripts/gc_stats.rb                      &&   
-git add	scripts/profiling_hungarian              &&   
-git add	test/functionals_extensions_test.rb      &&   
-git add	test/module_extensions_test.rb           &&   
-git add	test/object_extensions_test.rb           &&   
-git add	test/overlord.rb                         &&   
-git add	test/range_extensions_test.rb            &&   
-git add	test/symbol_extensions_test.rb           &&   
-git add	test/test_helper.rb                      &&   
-git add	test/unbound_method_extensions_test.rb        
