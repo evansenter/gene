6229f6a9435b3bb646c0c72621a7d7f8e4355bd0
Evan Senter <evansenter@gmail.com>
2009-11-19 22:59:27 -0800

    Pushed tests into a unit folder, removed overlord.rb because I wrote a rake task that does the same thing, better.

--- test/overlord.rb
+++
@@ -1,2 +0,0 @@
-# Run the tests! All of them!
-Dir.glob(File.join(File.dirname(__FILE__), "..", "test", "*_test.rb")).each { |test_file| print `ruby #{test_file}` }
--- test/unit/module_extensions_test.rb
+++ test/unit/module_extensions_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require File.join(File.dirname(__FILE__), "..", "test_helper.rb")
 
 class ModuleExtensionsTest < Test::Unit::TestCase
   def test_true
